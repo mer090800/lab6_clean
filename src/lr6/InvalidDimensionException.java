c347088118b3226677a07fc5e8b15f5a96746b1d
mer090800 <bandura.mariya.2004@gmail.com>
2026-04-19 19:44:27 +0200

    Delete src/lr6/InvalidDimensionException.java

--- src/lr6/InvalidDimensionException.java
+++
@@ -1,9 +0,0 @@
-package lr6;
-
-public class InvalidDimensionException extends Exception {
-
-    public InvalidDimensionException(String parameter, String value) {
-        super("Invalid value for parameter '" + parameter + "': " + value +
-                ". Value must be a positive number.");
-    }
-}
