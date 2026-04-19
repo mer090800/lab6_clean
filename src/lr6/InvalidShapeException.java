291bef33a9738601c974115251d0aada78c6d79c
mer090800 <bandura.mariya.2004@gmail.com>
2026-04-19 19:44:33 +0200

    Delete src/lr6 directory

--- src/lr6/InvalidShapeException.java
+++
@@ -1,9 +0,0 @@
-package lr6;
-
-public class InvalidShapeException extends Exception {
-
-    public InvalidShapeException(String shape) {
-        super("Unsupported shape: " + shape +
-                ". Available shapes: cube, cuboid, sphere, tetrahedron, torus, ellipsoid.");
-    }
-}
--- src/lr6/MissingParameterException.java
+++
@@ -1,8 +0,0 @@
-package lr6;
-
-public class MissingParameterException extends Exception {
-
-    public MissingParameterException(String parameter) {
-        super("Parameter '" + parameter + "' is missing.");
-    }
-}
